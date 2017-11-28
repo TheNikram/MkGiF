2cc78b54df377aaf9b7ee03cc29d6cd65184e28a
TheNikramuser.name <marcin6991@hormail.com>
2017-11-28 17:11:14 +0100

    Test

--- src/com/servlet/ConnectionServlet/Baza.java
+++ src/com/servlet/ConnectionServlet/Baza.java
@@ -9,7 +9,7 @@ import java.sql.Statement;
 import java.util.ArrayList;
 
 public class Baza {
-	/*public static void main( String args[] ) {
+	/*testpublic static void main( String args[] ) {
 	      Connection c = null;
 	      
 	      try {
