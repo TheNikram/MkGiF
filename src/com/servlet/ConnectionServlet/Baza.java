14ab59c432c88205fc2f9286bfdf4bc4ef2a5b59
TheNikram <marcin6991@hormail.com>
2017-12-04 18:39:35 +0100

    logowanie

--- src/com/servlet/ConnectionServlet/Baza.java
+++ src/com/servlet/ConnectionServlet/Baza.java
@@ -12,9 +12,9 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 public class Baza {
-	private String typyEventKsiazek[] = {"W przyszlosci", "W trakcie", "Skonczone", "Przerwana"};
-	private String typyEventFilmow[] = {"W przyszlosci", "W trakcie", "Skonczone", "Przerwana"};
-	private String typyEventGier[] = {"W przyszlosci", "W trakcie", "Skonczone", "Przerwana"};
+	private String typyEventKsiazek[] = {"W przyszlosci", "W trakcie", "Skonczone", "Przerwane"};
+	private String typyEventFilmow[] = {"W przyszlosci", "W trakcie", "Skonczone", "Przerwane"};
+	private String typyEventGier[] = {"W przyszlosci", "W trakcie", "Skonczone", "Przerwane"};
 	
 	public String getTypeEventKsiazek(int index)
 	{
@@ -58,7 +58,7 @@ public class Baza {
              ResultSet rs    = stmt.executeQuery(sql)){
             while (rs.next()) {
             	rezultat = rs.getInt("id");
-                System.out.println(rs.getInt("id"));
+                //System.out.println(rs.getInt("id"));
             }
             return rezultat;
         } catch (SQLException e) {
@@ -88,13 +88,12 @@ public class Baza {
     
     public JSONArray zestawienieKsiazek(String typ, int idUzytkownik)
     {
-    	ArrayList<String> tablica = new ArrayList<String>();
-    	String sql = "SELECT BookEvent.Date AS Data, Book.Title AS Tytul, Book.Creator AS Tworca, "
+    	String sql = "SELECT Book.ID AS ID, BookEvent.Date AS Data, Book.Title AS Tytul, Book.Creator AS Tworca, "
     			+ "Rating.Rating AS Rating, BookEventType.Name AS Zdarzenie FROM BookEvent INNER JOIN Book "
     			+ "ON BookEvent.ID_Book = Book.ID INNER JOIN BookEventType ON BookEventType.ID = BookEvent.ID_BookEventType "
     			+ "INNER JOIN Rating ON Rating.ID = Book.ID_Rating WHERE BookEvent.ID_User = " + idUzytkownik
     			+ " AND BookEventType.Name = '" + typ + "'";
-    	System.out.println(sql);
+    	//System.out.println(sql);
     	JSONObject json;
     	JSONArray array = new JSONArray();
         try (Connection conn = this.connect();
@@ -103,12 +102,13 @@ public class Baza {
             while (rs.next()) {
             	json = new JSONObject();
             	//tablica.add(rs.getString("tytul"));
-                System.out.println(rs.getString("Data") +  "\t" + 
-                                   rs.getString("Tytul") + "\t" +
-                                   rs.getString("Tworca") + "\t" +
-                                   rs.getString("Rating") + "\t" +
-                                   rs.getString("Zdarzenie") + "\n");
-                json.put("Data", rs.getString("Data"));
+                //System.out.println(rs.getString("Data") +  "\t" + 
+                  //                 rs.getString("Tytul") + "\t" +
+                    //               rs.getString("Tworca") + "\t" +
+                      //             rs.getString("Rating") + "\t" +
+                        //           rs.getString("Zdarzenie") + "\n");
+                json.put("ID", rs.getString("ID"));
+            	json.put("Data", rs.getString("Data"));
                 json.put("Tytul", rs.getString("Tytul"));
                 json.put("Tworca", rs.getString("Tworca"));
                 json.put("Rating", rs.getString("Rating"));
@@ -123,13 +123,12 @@ public class Baza {
     
     public JSONArray zestawienieFilmow(String typ, int idUzytkownik)
     {
-    	ArrayList<String> tablica = new ArrayList<String>();
     	String sql = "SELECT MovieEvent.Date AS Data, Movie.Title AS Tytul, Movie.Creator AS Tworca, "
     			+ "Rating.Rating AS Rating, MovieEventType.Name AS Zdarzenie FROM MovieEvent INNER JOIN Movie "
     			+ "ON MovieEvent.ID_Movie = Movie.ID INNER JOIN MovieEventType ON MovieEventType.ID = MovieEvent.ID_MovieEventType "
     			+ "INNER JOIN Rating ON Rating.ID = Movie.ID_Rating WHERE MovieEvent.ID_User = " + idUzytkownik
     			+ " AND MovieEventType.Name = '" + typ + "'";
-    	System.out.println(sql);
+    	//System.out.println(sql);
     	JSONObject json;
     	JSONArray array = new JSONArray();
         try (Connection conn = this.connect();
@@ -158,13 +157,12 @@ public class Baza {
     
     public JSONArray zestawienieGier(String typ, int idUzytkownik)
     {
-    	ArrayList<String> tablica = new ArrayList<String>();
     	String sql = "SELECT GameEvent.Date AS Data, Game.Title AS Tytul, Game.Creator AS Tworca, "
     			+ "Rating.Rating AS Rating, GameEventType.Name AS Zdarzenie FROM GameEvent INNER JOIN Game "
     			+ "ON GameEvent.ID_Game = Game.ID INNER JOIN GameEventType ON GameEventType.ID = GameEvent.ID_GameEventType "
     			+ "INNER JOIN Rating ON Rating.ID = Game.ID_Rating WHERE GameEvent.ID_User = " + idUzytkownik
     			+ " AND GameEventType.Name = '" + typ + "'";
-    	System.out.println(sql);
+    	//System.out.println(sql);
     	JSONObject json;
     	JSONArray array = new JSONArray();
         try (Connection conn = this.connect();
@@ -190,6 +188,41 @@ public class Baza {
         }
 		return array;
     }
+    
+    public void edytowanieRatinguKsiazki(int idKsiazki, int nowaOcena, int idUzytkownik)
+    {
+    	String sql = "SELECT * FROM Rating WHERE ID = (SELECT ID_Rating FROM Book WHERE ID = " + idKsiazki + ")";
+    	String suma, ilosc, idRating;
+    	System.out.println(sql);
+        try (Connection conn = this.connect();
+             Statement stmt  = conn.createStatement();
+             ResultSet rs    = stmt.executeQuery(sql)){
+            while (rs.next()) {
+            	idRating = rs.getString("ID");
+            	ilosc = rs.getString("Ilosc");
+            	suma = rs.getString("Suma");
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return;
+        }
+        sql = "SELECT COUNT (*) AS Rezultat FROM BookEvent INNER JOIN BookEventType ON BookEventType.ID =  BookEvent.ID_BookEventType WHERE ID_User = " + idUzytkownik + " AND BookEventType.Name = 'Rating'";
+        //int rezultat = 
+        try (Connection conn = this.connect();
+                Statement stmt  = conn.createStatement();
+                ResultSet rs    = stmt.executeQuery(sql)){
+        	   while (rs.next()) {
+               	idRating = rs.getString("Rezultat");
+               	ilosc = rs.getString("Ilosc");
+               	suma = rs.getString("Suma");
+        	   }
+           
+           sql = "UPDATE Rating SET Suma = 12, Ilosc = 3, Rating = 4 WHERE ID = 1";
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
     public static void main(String[] args) {
         Baza app = new Baza();
         //app.selectAll();
--- src/com/servlet/ConnectionServlet/ConnectionSwervlet.java
+++ src/com/servlet/ConnectionServlet/ConnectionSwervlet.java
@@ -19,7 +19,7 @@ import org.json.simple.JSONObject;
 @WebServlet("/ConnectionSwervlet")
 public class ConnectionSwervlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+	private static int  licznik = 0;
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -32,30 +32,48 @@ public class ConnectionSwervlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		licznik++;
 		// TODO Auto-generated method stub
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		String login, password;
+		
+		if ((request.getParameter("Login") == null) || (request.getParameter("Password") == null))
+		{
+			login = password = "";
+			response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+			
+		}
+		else
+		{
+			login = request.getParameter("Login");
+			password = request.getParameter("Password");
+		}
 		JSONObject json = new JSONObject();
 		JSONObject jsonGlowny = new JSONObject();
 		Baza b = new Baza();
 //ArrayList<String> filmy= b.selectAll();
-		//b.sprawdzenieUzytkownika("Nikram", "nikram");
-        json.put(b.getTypeEventKsiazek(0), b.zestawienieKsiazek(b.getTypeEventKsiazek(0), b.sprawdzenieUzytkownika("Nikram", "nikram")));
-        json.put(b.getTypeEventKsiazek(1), b.zestawienieKsiazek(b.getTypeEventKsiazek(1), b.sprawdzenieUzytkownika("Nikram", "nikram")));
-        json.put(b.getTypeEventKsiazek(2), b.zestawienieKsiazek(b.getTypeEventKsiazek(2), b.sprawdzenieUzytkownika("Nikram", "nikram")));
-        json.put(b.getTypeEventKsiazek(3), b.zestawienieKsiazek(b.getTypeEventKsiazek(3), b.sprawdzenieUzytkownika("Nikram", "nikram")));
-        jsonGlowny.put("Ksiazki", json);
+		if (b.sprawdzenieUzytkownika(login, password) == -1)
+		{
+			login = password = "";
+			response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+		}
+        json.put(b.getTypeEventKsiazek(0), b.zestawienieKsiazek(b.getTypeEventKsiazek(0), b.sprawdzenieUzytkownika(login, password)));
+        json.put(b.getTypeEventKsiazek(1), b.zestawienieKsiazek(b.getTypeEventKsiazek(1), b.sprawdzenieUzytkownika(login, password)));
+        json.put(b.getTypeEventKsiazek(2), b.zestawienieKsiazek(b.getTypeEventKsiazek(2), b.sprawdzenieUzytkownika(login, password)));
+        json.put(b.getTypeEventKsiazek(3), b.zestawienieKsiazek(b.getTypeEventKsiazek(3), b.sprawdzenieUzytkownika(login, password)));
+        jsonGlowny.put("Ksiazka", json);
         json = new JSONObject();
-        json.put(b.getTypeEventFilmow(0), b.zestawienieFilmow(b.getTypeEventFilmow(0), b.sprawdzenieUzytkownika("Nikram", "nikram")));
-        json.put(b.getTypeEventFilmow(1), b.zestawienieFilmow(b.getTypeEventFilmow(1), b.sprawdzenieUzytkownika("Nikram", "nikram")));
-        json.put(b.getTypeEventFilmow(2), b.zestawienieFilmow(b.getTypeEventFilmow(2), b.sprawdzenieUzytkownika("Nikram", "nikram")));
-        json.put(b.getTypeEventFilmow(3), b.zestawienieFilmow(b.getTypeEventFilmow(3), b.sprawdzenieUzytkownika("Nikram", "nikram")));
-        jsonGlowny.put("Filmy", json);
+        json.put(b.getTypeEventFilmow(0), b.zestawienieFilmow(b.getTypeEventFilmow(0), b.sprawdzenieUzytkownika(login, password)));
+        json.put(b.getTypeEventFilmow(1), b.zestawienieFilmow(b.getTypeEventFilmow(1), b.sprawdzenieUzytkownika(login, password)));
+        json.put(b.getTypeEventFilmow(2), b.zestawienieFilmow(b.getTypeEventFilmow(2), b.sprawdzenieUzytkownika(login, password)));
+        json.put(b.getTypeEventFilmow(3), b.zestawienieFilmow(b.getTypeEventFilmow(3), b.sprawdzenieUzytkownika(login, password)));
+        jsonGlowny.put("Film", json);
         json = new JSONObject();
-        json.put(b.getTypeEventGier(0), b.zestawienieGier(b.getTypeEventGier(0), b.sprawdzenieUzytkownika("Nikram", "nikram")));
-        json.put(b.getTypeEventGier(1), b.zestawienieGier(b.getTypeEventGier(1), b.sprawdzenieUzytkownika("Nikram", "nikram")));
-        json.put(b.getTypeEventGier(2), b.zestawienieGier(b.getTypeEventGier(2), b.sprawdzenieUzytkownika("Nikram", "nikram")));
-        json.put(b.getTypeEventGier(3), b.zestawienieGier(b.getTypeEventGier(3), b.sprawdzenieUzytkownika("Nikram", "nikram")));
-        jsonGlowny.put("Gry", json);
+        json.put(b.getTypeEventGier(0), b.zestawienieGier(b.getTypeEventGier(0), b.sprawdzenieUzytkownika(login, password)));
+        json.put(b.getTypeEventGier(1), b.zestawienieGier(b.getTypeEventGier(1), b.sprawdzenieUzytkownika(login, password)));
+        json.put(b.getTypeEventGier(2), b.zestawienieGier(b.getTypeEventGier(2), b.sprawdzenieUzytkownika(login, password)));
+        json.put(b.getTypeEventGier(3), b.zestawienieGier(b.getTypeEventGier(3), b.sprawdzenieUzytkownika(login, password)));
+        jsonGlowny.put("Gra", json);
 		response.setContentType("application/json");
 		PrintWriter out = response.getWriter();
 //		String message;
@@ -78,9 +96,19 @@ public class ConnectionSwervlet extends HttpServlet {
 //		json.put("Filmy", array);
 //		message = json.toString();
 //
+		if (b.sprawdzenieUzytkownika(request.getParameter("Login") ,request.getParameter("Password")) == -1)
+		{
+			jsonGlowny = new JSONObject();
+		}
 		out.print(jsonGlowny);
 		out.flush();
-	}
+		System.out.println("Licznik:" + licznik + "\t" );
+		for(String string : request.getParameterMap().keySet()) {
+			System.out.println(string + "  " + request.getParameter(string));
+
+		}
+		
+		}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
